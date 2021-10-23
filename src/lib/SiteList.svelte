7e7f784d188701fcef2b1b33834a2f94131b465a
David Hunt <git@dnotes.net>
2021-10-23 15:45:41 -0700

    fixed display on memorial pages

--- src/lib/SiteList.svelte
+++ src/lib/SiteList.svelte
@@ -4,7 +4,8 @@
 </script>
 
 <div class="flex flex-wrap justify-center overflow-hidden max-w-3xl mx-auto">
+  <a class="block px-2 w-full leading-none" href="{base}/sites/"><h3 class="border-b-2 pb-2 mb-2">Memorials</h3></a>
   {#each sites as s}
-    <a class="inline-block w-full overflow-hidden px-2 py-1" style="max-width:250px" href="{base}/sites/{s.slug}">{s.name}</a>
+    <a class="inline-block w-full overflow-hidden px-2 py-1" style="max-width:244px" href="{base}/sites/{s.slug}">{s.name}</a>
   {/each}
 </div>
--- src/routes/sites/[slug].svelte
+++ src/routes/sites/[slug].svelte
@@ -13,36 +13,48 @@
 
 <script lang="ts">
 import Image from "$lib/Image.svelte";
+import SiteList from "$lib/SiteList.svelte";
 import type { Gravesite } from "src/global";
   export let site:Gravesite
-  let stats = [site?.born ?? `b. ${site.born}`, site?.deceased ?? `d. ${site.deceased}`].filter(Boolean)
+  let stats = [site?.born && (`b. ${site.born}`), site?.deceased && (`d. ${site.deceased}`)].filter(Boolean)
 </script>
 
 <svelte:head>
   <title>{site.name} | Desert Rose Memorial Garden</title>
 </svelte:head>
 
-<div class="text-center max-w-2xl mx-auto">
+<div class="flex flex-wrap justify-center">
 
-  <h2>{site.name}</h2>
+  <div class="max-w-full mb-8" style="width:672px;">
+    <div class="text-center">
 
-  {#if site.image}
-    <Image image="/sites/{site.image}" alt="{site.name}" class="w-2xl mx-auto" />
-  {/if}
+      <h2>{site.name}</h2>
 
-  {#if stats.length}
-    <p>
-      {#each stats as item,i}
-        {#if i}
-          <br>
-        {/if}
-        {item}
-      {/each}
-    </p>
-  {/if}
+      {#if site.image}
+        <Image image="/sites/{site.image}" alt="{site.name}" class="w-2xl mx-auto" />
+      {/if}
+
+      {#if stats.length}
+        <p>
+          {#each stats as item,i}
+            {#if i}
+              <br>
+            {/if}
+            {item}
+          {/each}
+        </p>
+      {/if}
+
+    </div>
+
+    <div>
+      {@html site.html}
+    </div>
+  </div>
+
+  <div class="lg:w-64 lg:text-right lg:ml-8">
+    <SiteList />
+  </div>
 
 </div>
 
-<div class="max-w-2xl mx-auto">
-  {@html site.html}
-</div>
